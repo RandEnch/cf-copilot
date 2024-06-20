aac063532e56e95afe8fe29ea90adb6f4dee1426
smaa <afeee@qq.com>
2024-06-20 09:32:52 +0800

    Update pages.ts

--- src/pages.ts
+++ src/pages.ts
@@ -2,5 +2,11 @@ import { bingPorxyWorker } from "./proxy/bingPorxyWorker";
 
 export async function onRequest(context:EventContext<Env,string,any>):Promise<Response>{
     const { request, env } = context;
-    return bingPorxyWorker(request, env);
+    const { pathname } = new URL(request.url);
+    if (pathname == "/robots.txt") {
+        return new Response("User-agent: *\nDisallow: /", {
+            headers: { "Content-Type": "text/plain" },
+        });
+    } else
+	return bingPorxyWorker(request, env);
 }
