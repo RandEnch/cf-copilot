f3583eeca3a882378ab83c84e7ad643eb17f9891
RandEnch <120745557+RandEnch@users.noreply.github.com>
2025-09-20 03:13:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ export default {
   async fetch(request) {
     const url = new URL(request.url)
     url.hostname = "copilot.microsoft.com"
-    return fetch(new Request(url, request))
+    url.protocol = "https:"
+    const newReq = new Request(url, request)
+    newReq.headers.set("host", "copilot.microsoft.com")
+    return fetch(newReq)
   }
 }
